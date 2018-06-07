b36f5fb9fc0dabf82bbbc6e6d051e0a133560c8d
Toni Petrina <tonipetrina@hotmail.com>
2018-06-07 22:43:03 +0200

    Update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter Blog",
-    author: "Kyle Mathews",
+    title: "GigPin Engineering",
+    author: "GigPin",
   },
   plugins: [
     {
